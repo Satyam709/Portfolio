cfb56bdea9c4ab34f1a29e7ddb4869589bbe2de2
satyam709 <satyamchauhan709@gmail.com>
2025-03-03 21:42:31 +0530

    fixed theme viewport metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,14 @@
-import type { Metadata } from 'next'
+import type { Metadata, Viewport } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
   title: 'Satyam',
-  description: 'Satyam\'s portfolio',
-  themeColor: "#0C0C0D",
+  description: 'Satyam\'s portfolio',  
 }
+
+export const viewport: Viewport = {
+  themeColor: "#0C0C0D",
+};
 
 export default function RootLayout({
   children,
